0706a85694e3a94210b7c8cd38c24c7385c4e5de
Tyberion <alexis.vigoureux@gmail.com>
2016-12-15 00:53:58 +0100

    Page inscription

---
+++ src/main/java/com/utbm/formation/controller/AddCourseSessionController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utbm.formation.controller;
+
+import com.utbm.lo54.entity.Course;
+import com.utbm.lo54.entity.Course_session;
+import com.utbm.lo54.entity.Location_course;
+import com.utbm.lo54.service.CourseService;
+import com.utbm.lo54.service.CourseSessionService;
+import com.utbm.lo54.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author jnoblat
+ */
+@Controller
+@RequestMapping(value = "/addCourseSession")
+public class AddCourseSessionController {
+    
+    @Autowired
+    CourseSessionService courseSessionService;
+    
+    @Autowired
+    CourseService courseService;
+    
+    @Autowired
+    LocationService locationService;
+    
+    @RequestMapping(value = "", method = GET)
+    public ModelAndView addCourseSession( Model model) {
+        
+        Iterable<Course_session> listSession = courseSessionService.getAllCoursesSession();
+        Iterable<Course> listCourse = courseService.getAllCourses();
+        Iterable<Location_course> listLocation = locationService.getAllLocation();
+        
+        
+        model.addAttribute("listCourse",  listCourse);
+        model.addAttribute("listLocation",  listLocation);
+        return new ModelAndView("addCourseSession", model.asMap());
+    }
+ 
+}
