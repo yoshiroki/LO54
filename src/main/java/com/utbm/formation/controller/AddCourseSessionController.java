cc076425743007ce9fbc951130885e23302650b9
Alexis Vigoureux <avigoure@gi.utbm.fr>
2016-12-16 17:59:40 +0100

    derniere correction

---
+++ src/main/java/com/utbm/formation/controller/AddCourseSessionController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utbm.formation.controller;
+
+import com.utbm.lo54.service.CourseService;
+import com.utbm.lo54.service.CourseSessionService;
+import com.utbm.lo54.service.LocationService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author jnoblat
+ */
+@Controller
+@RequestMapping(value = "/addCourseSession")
+public class AddCourseSessionController {
+    
+    @Autowired
+    CourseSessionService courseSessionService;
+    
+    @Autowired
+    CourseService courseService;
+    
+    @Autowired
+    LocationService locationService;
+    
+    @RequestMapping(value = "", method = POST)
+    public ModelAndView listCourse( @RequestParam("selectCourse") List<String> data, Model model) {
+        
+        System.out.println(data);
+        Iterable<String> selectedCourse = data;
+        
+//        Iterable<Course_session> listSession = courseSessionService.getAllCoursesSession();
+
+
+//        model.addAttribute("listLocation",  listLocation);
+        model.addAttribute("selectedCourse", selectedCourse);
+        return new ModelAndView("addCourseSession", model.asMap());
+    } 
+}
---
+++ src/main/java/com/utbm/formation/controller/ValidInsertSessionController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utbm.formation.controller;
+
+import com.utbm.lo54.service.CourseService;
+import com.utbm.lo54.service.CourseSessionService;
+import com.utbm.lo54.service.LocationService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author jnoblat
+ */
+@Controller
+@RequestMapping(value = "/validInsertSession")
+public class ValidInsertSessionController {
+    
+    @Autowired
+    CourseSessionService courseSessionService;
+    
+    @Autowired
+    CourseService courseService;
+    
+    @Autowired
+    LocationService locationService;
+    
+    @RequestMapping(value = "", method = POST)
+    public ModelAndView listCourse( Model model) {
+        
+ 
+        return new ModelAndView("validInsertSession", model.asMap());
+    }
+ 
+}
